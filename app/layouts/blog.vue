5a772898522021d72c6a6786c7745d4411353a7d
shadowdreamer <git@dovahkiin.top>
2025-03-15 18:27:38 +0800

    🔧 build:

--- app/layouts/blog.vue
+++ app/layouts/blog.vue
@@ -2,22 +2,23 @@
   SiteNav
   main(class='main-container mx-auto')
     NuxtPage
-    Giscus(
-      id="comments"
-      repo="shadowdreamer/nuxt-home"
-      repo-id="R_kgDON4978A"
-      category="Announcements"
-      category-id="DIC_kwDON4978M4CoDr8"
-      mapping="pathname"
-      strict="0"
-      reactions-enabled="1"
-      emit-metadata="0"
-      input-position="top"
-      theme="light"
-      lang="zh-CN"
-      loading="lazy"
-      crossorigin="anonymous"
-    )
+    ClientOnly
+      Giscus(
+        id="comments"
+        repo="shadowdreamer/nuxt-home"
+        repo-id="R_kgDON4978A"
+        category="Announcements"
+        category-id="DIC_kwDON4978M4CoDr8"
+        mapping="pathname"
+        strict="0"
+        reactions-enabled="1"
+        emit-metadata="0"
+        input-position="top"
+        theme="light"
+        lang="zh-CN"
+        loading="lazy"
+        crossorigin="anonymous"
+      )
   </template>
 <script setup lang="ts">
 import Giscus from "@giscus/vue";
