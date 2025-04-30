d69bcf7825266ef8f0f452270705e75ee7bcdf07
shadowdreamer <git@dovahkiin.top>
2025-04-30 16:13:44 +0800

    refactor: enhance profile component layout, add new social links, and improve responsiveness

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 SiteNav
 main(class='container mx-auto')
-  .flex.gap-8.items-start.relative
+  .flex.gap-8.items-start.relative(class="max-md:flex-wrap")
     
     NuxtPage
     SideProfile.sticky.top-20
